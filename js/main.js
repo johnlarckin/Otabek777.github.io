2e3cfc1409ad15200d093e6c0b2b8e8743eda955
John Larckin <johnlarckin@gmail.com>
2020-06-04 21:06:27 +0500

    готово

--- js/main.js
+++ js/main.js
@@ -1,2 +1,31 @@
 'use strict';
 
+// hide stick menu
+
+let header = document.querySelector('.nav');
+
+$(window).scroll(function() {
+
+    if($(this).scrollTop() > 100) {    
+        header.classList.add('sticky');
+    } else {
+        header.classList.remove('sticky');
+    }
+});
+
+let navSub = document.querySelector('.header-sub');
+
+let navItem = document.querySelector('#nav-item').addEventListener('click', function (e) {
+    e.preventDefault();
+    navSub.classList.toggle('header-sub-active'); 
+});
+
+navSub.addEventListener('click', function (e) {
+    if (e.target === navSub) {
+        this.classList.remove('header-sub-active');
+    }
+})
+
+$('.article__carousel').slick({
+    infinite: true,
+});
